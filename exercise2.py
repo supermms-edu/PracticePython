b70da67ffc18fe180761c997237897081d1fff03
supermms-edu <74331231+supermms-edu@users.noreply.github.com>
2020-12-14 14:36:25 -0300

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+## Exercise from  https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+
+x = int(input("Enter a number\n"))
+ 
+if x%2 == 0:
+  print("It's an even number!")
+else:
+  print("It's an odd number!")
+
+
+
